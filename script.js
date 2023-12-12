7ed07ec1246b888b4c32b062bc037a7c634aa9ea
Luiz Gustavo <lgnovais07@gmail.com>
2023-12-11 22:47:29 -0300

    footer e paddigns

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-const linksInternos = document.querySelectorAll(".header_container ul li a[href^='#']")
-console.log(linksInternos)
+function scrollSuave(){
+    const linksInternos = document.querySelectorAll(".header_container ul li a[href^='#']")
 
-function scrollToSection(event){
-    event.preventDefault()
-    const href = event.currentTarget.getAttribute("href")
-    const section = document.querySelector(href)
-    section.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-    })
-    console.log(section)
+    if(linksInternos.length){
+        function scrollToSection(event){
+            event.preventDefault()
+            const href = event.currentTarget.getAttribute("href")
+            const section = document.querySelector(href)
+            section.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+            console.log(section)
+        }
+        
+        
+        linksInternos.forEach((link) => {
+            link.addEventListener('click', scrollToSection)
+        })
+    }
 }
+scrollSuave()
 
+function initAnimaScroll(){
+    const sections = document.querySelectorAll('.js .js-scroll');
+    const windowHeight = window.innerHeight * 0.6
+    if(sections.length){
+        function animaScroll(){
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top - windowHeight;
+                if(sectionTop < 0){
+                    section.classList.add('ativo')
+                }
+            })
+        }
+        animaScroll()
 
-linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection)
-})
+        window.addEventListener('scroll', animaScroll)
+    }
+}
+initAnimaScroll()
